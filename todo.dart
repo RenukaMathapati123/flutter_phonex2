9d0822fa8cc9a868c0e2f36d847b8f5ebeb48229
RenukaMathapati123 <110781726+RenukaMathapati123@users.noreply.github.com>
2022-11-09 07:20:45 +0530

    Add files via upload

---
+++ todo.dart
@@ -0,0 +1,12 @@
+class Todo {
+  final String customerName;
+  final String email;
+  final String pickupAddress;
+  
+  Todo({
+    required this.customerName,
+    required this.email,
+    required this.pickupAddress,
+    
+  });
+}
