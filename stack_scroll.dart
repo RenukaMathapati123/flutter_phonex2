958a72223bccf84a4acf16f4e5dc3fff8e84a408
RenukaMathapati123 <110781726+RenukaMathapati123@users.noreply.github.com>
2022-11-10 17:34:12 +0530

    Add files via upload

---
+++ stack_scroll.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class StackScroll extends StatefulWidget {
+  const StackScroll({super.key});
+
+  @override
+  State<StackScroll> createState() => _StackScrollState();
+}
+
+class _StackScrollState extends State<StackScroll> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
